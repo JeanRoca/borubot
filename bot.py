5a23c694697b5f60bf11366ffc09dafb6b149a31
JeanRoca <33772802+JeanRoca@users.noreply.github.com>
2018-07-16 23:42:01 -0500

    Changed !boruto command
    Changed boruto command to now connect to webpage in order to ensure latest episode retreival

--- bot.py
+++ bot.py
@@ -4,24 +4,6 @@ from bs4 import BeautifulSoup as soup
 client = discord.Client()
 
 my_url = "https://www.watchcartoononline.io/anime/boruto-naruto-next-generations-english-subbed"
-uClient = uReq(my_url)
-page_html = uClient.read()
-
-# close connection after we have read information
-uClient.close()
-
-# html parsing
-page_soup = soup(page_html, "html.parser")
-
-# grabs all of the Boruto episodes
-containers = page_soup.findAll("a", {"class": "sonra"})
-
-# grab the title from the newest episode indexed at 0
-latest_episode = containers[0].text
-
-# grab the latest link
-latest_link = str(str(containers).split("href=", )[1]).split("rel=")[0].replace('"', "").strip()
-
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
@@ -33,9 +15,26 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('!boruto'):
+        uClient = uReq(my_url)
+        page_html = uClient.read()
+
+        # close connection after we have read information
+        uClient.close()
+
+        # html parsing
+        page_soup = soup(page_html, "html.parser")
+
+        # grabs all of the Boruto episodes
+        containers = page_soup.findAll("a", {"class": "sonra"})
+
+        # grab the title from the newest episode indexed at 0
+        latest_episode = containers[0].text
+
+        # grab the latest link
+        latest_link = str(str(containers).split("href=", )[1]).split("rel=")[0].replace('"', "").strip()
+
         msg = (latest_episode + "                       " + latest_link).format(message)
         await client.send_message(message.channel, msg)
-
 
 @client.event
 async def on_ready():
